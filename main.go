b9f1479d88cb563c1ddca78acc096572bb9780b5
skurkov <p.skurkov@tendertech.ru>
2018-09-28 17:24:35 +0300

    Неотформатированный вывод структуры

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io"
+	//"io"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+var ignoredFiles = []string{".git", "dockerfile",".","hw1.md"}
 
 func main() {
 	out := os.Stdout
@@ -20,3 +22,50 @@ func main() {
 		panic(err.Error())
 	}
 }
+
+func dirTree(out interface{}, filePath string, printFiles bool) (error) {
+
+	var prinPath string
+	var pathList []string
+
+
+	err := filepath.Walk(filePath, func(path string, file os.FileInfo, err error) error {
+
+		if isFileIgnored(path) || (!file.IsDir() && !printFiles) {
+			return nil
+		}
+
+		pathList = append(pathList, path)
+
+		for _,line := range pathList{
+
+			//тут нужно както отформатировать вывод
+			prinPath += line+"\t\n"
+		}
+
+		fmt.Println(prinPath)
+
+
+		return nil
+	})
+	return err
+
+}
+
+
+func isFileIgnored(path string) bool{
+	currentPathList :=strings.Split(path, string(os.PathSeparator))
+
+
+	for _,ignored := range  ignoredFiles {
+
+			if currentPathList[0] == ignored {
+
+				return true
+			}
+
+	}
+	return false
+}
+
+func getAllPaths(){}
